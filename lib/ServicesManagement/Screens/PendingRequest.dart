672580fccf45e3c384df44830c6297a20cf51910
DiegoPorta20 <u202019287@upc.edu.pe>
2023-10-30 16:42:14 -0500

    request

--- lib/ServicesManagement/Screens/PendingRequest.dart
+++ lib/ServicesManagement/Screens/PendingRequest.dart
@@ -56,10 +56,74 @@ class _PendingRequestState extends State<PendingRequest> {
       setState(() {
         request = request;
         requests = requests;
+        showCustomDialog(context, "Felicidades", "Envío de solicitud exitoso", true);
       });
+    }else{
+      showCustomDialog(context, "Algo Salió Mal", "Error al enviar su solicitud", false);
     }
   }
-
+  void showCustomDialog(BuildContext context, String title, String content, bool isSuccess) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(16.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 10.0,
+                  offset: const Offset(0.0, 10.0),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Icon(
+                  isSuccess ? Icons.check_circle : Icons.error,
+                  color: isSuccess ? Colors.green : Colors.red,
+                  size: 50.0,
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  content,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 16.0),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
